b850fdc3bf5ef29521b042cc95d0f61f2ed6d21b
도연 김 <kdy5194@cu.ac.kr>
2019-12-10 22:03:21 +0900

    ㅁㄴㅇ

---
+++ Project1/Project1/소스.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	double degree[] = { -5.34, 3.67, 71.76, 28.76, 35.63 };
+
+	int size = sizeof(degree) / sizeof(double);
+
+	for (int i = 0; i < size; i++) {
+		printf("%.2lf ", degree[i]);
+	}
+	puts("");
+}
